9b8ede2b7c16737bc26fce8db07b098449a3f4af
Lauren Piontek <piontekle@gmail.com>
2018-10-10 02:09:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,9 +20,8 @@ function add2(n) {
   }
 
 var funkyFunction = function() {
-  return function() {
-    return "FUNKY!"
-  }
+      return "FUNKY!"
+  
 }
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
